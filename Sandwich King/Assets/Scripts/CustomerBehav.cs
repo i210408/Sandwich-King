6c2e54ca9b2075a5595fe202cb8bab9a3f96d3bc
LeenaRizwan <i210430@nu.edu.pk>
2024-07-26 02:58:06 +0500

    Meat Assets Added In

--- Sandwich King/Assets/Scripts/CustomerBehav.cs
+++ Sandwich King/Assets/Scripts/CustomerBehav.cs
@@ -6,11 +6,7 @@ using TMPro;
 public class CustomerBehav : MonoBehaviour
 {
     public List<string> Order;//To store the three order requests
-    public int Score=0;//The total obtainable score is out of 5 for each compoent the player gets correct
-    //In the case that the, say, sauces, dont matter, all possible sauces will be in the AcceptableList,
-    //so the player gets an fair score count for the ones that the hints dont necessarily hint at
-    //Eg: Hints are "Spicy + No Meat + Healthy Bread", doesn't tell you anything abt the sauces or cheeses to use
-    //therefore, any choice for those is fine
+    public int Score=0;
     public List<string> Acceptable;
 public TextMeshPro Textbox;
     // Start is called before the first frame update
@@ -30,7 +26,5 @@ public TextMeshPro Textbox;
         if(Input.GetMouseButtonDown(0)){
             Textbox.text= "Please make it quick";
         }
-        //For now, if tap, delete, then spawn next, until Total is done. or maybe that code should be in the spawner instead? 
-        //Maybe. Yes, i think so
     }
 }
