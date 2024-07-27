00d1e075f75aae5687584962ae90a71eca5bc880
Enigmatic619 <azhanmuhammad123@gmail.com>
2024-07-27 22:21:00 +0500

    Main menu basic with a bit of resolution
    ayo tell me whats next and if these need change

---
+++ Sandwich King/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainmenuButtons : MonoBehaviour
+{
+    public Button playButton;
+    public Button tutorialButton;
+
+    void Start()
+    {
+     
+
+        // Add listeners to the buttons
+        playButton.onClick.AddListener(OnPlayButtonPressed);
+        tutorialButton.onClick.AddListener(OnTutorialButtonPressed);
+    }
+
+    void OnPlayButtonPressed()
+    {
+        Debug.Log("Play button has been pressed.");
+    }
+
+    void OnTutorialButtonPressed()
+    {
+        Debug.Log("Tutorial button has been pressed.");
+    }
+}
